c97ba41ed9f930960bd07f8bf7246924ed9de349
21Geral <geraldinerevillamar@gmail.com>
2025-09-05 15:38:05 -0500

    dark mode

---
+++ js/geraldine.js
@@ -0,0 +1,3 @@
+let darkmode = document.querySelector("#darkmode");
+let bottom = document.querySelector("#toggle");
+export { darkmode, bottom };
---
+++ js/main.js
@@ -0,0 +1,4 @@
+import { darkmode, bottom } from "./geraldine.js";
+bottom.addEventListener("click", function () {
+  darkmode.classList.toggle("dark");
+});
